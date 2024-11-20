8eafd14bedb0c6bffb1764d6b2fb3a2ba08233bf
ViethoangMai-dev <viethoangmai21199@.com>
2024-11-20 16:18:29 +0700

    add Formsearch demo in Favorite

--- src/pages/Favorite/ListFavorites.jsx
+++ src/pages/Favorite/ListFavorites.jsx
@@ -5,10 +5,23 @@ import PropTypes from "prop-types";
 import FavoriteBtn from "../../components/FavoriteBtn";
 import { login } from "../../stores/slices/authSlice";
 export default function ListFavorites({ listProduct: data }) {
-    console.log(data);
-
     return (
         <section className="product py-10">
+            <div className="flex flex-wrap items-center justify-between py-5">
+                <h4 className="text-lg font-semibold mb-5">All Items</h4>
+                <div className="filter ">
+                    <form action="" className=" flex items-center ">
+                        <input
+                            type="text"
+                            className="peer text-xs font-medium h-[35px] rounded-l-full border border-gray-500 outline-none px-3 w-[200px] py-1 border-r-0 focus:outline-blue-500 "
+                            placeholder="Search your favorite"
+                        />
+                        <button className=" peer-focus:bg-gray-600 transition-all duration-200 peer-focus:text-white py-1 px-2 h-[35px] rounded-r-full border border-gray-500 border-l-0">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
             <ul className="list grid grid-cols-4 gap-8 xl:gap-5 md:grid-cols-3 sm:grid-cols-2 sm:gap-3 ">
                 {data?.map(({ product }) => (
                     <div key={product.id} className="relative group  ">
